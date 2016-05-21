02713be8da421a29d8a99b307303dde49cff8f3b
Зебзеев Артемий Николаевич <Zebzeev_AN@uralkali.com>
2016-05-21 14:56:53 +0500

    tour of heroes

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+  {'id': 11, 'name': 'Hero 1'},
+  {'id': 12, 'name': 'Hero 2'},
+  {'id': 13, 'name': 'Hero 3'},
+  {'id': 14, 'name': 'Hero 4'},
+  {'id': 15, 'name': 'Hero 5'},
+  {'id': 16, 'name': 'Hero 6'},
+  {'id': 17, 'name': 'Hero 7'},
+  {'id': 18, 'name': 'Hero 8'},
+  {'id': 19, 'name': 'Hero 9'},
+  {'id': 20, 'name': 'Hero20'}
+];
